1b02119be18bf36871668e93161950e6ae0ac3b6
aCute5 <costantini95dario@gmail.com>
2022-11-24 15:33:47 +0100

    trying to run the server

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,10 @@
 
 <script>
 import headerPage from "@/components/headerPage.vue";
-import mainPage from "@/components/mainPage.vue";
-import footerPage from "@/components/footerPage.vue";
 
 export default {
   components: {
     headerPage,
-    mainPage,
-    footerPage,
   },
 };
 </script>
