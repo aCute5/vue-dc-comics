ec7ebc588750f0357a41a0086604200a3436f2fd
aCute5 <costantini95dario@gmail.com>
2022-11-24 15:32:35 +0100

    formate base project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <headerPage></headerPage>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import headerPage from "@/components/headerPage.vue";
+import mainPage from "@/components/mainPage.vue";
+import footerPage from "@/components/footerPage.vue";
 
 export default {
-  name: "App",
   components: {
-    HelloWorld,
+    headerPage,
+    mainPage,
+    footerPage,
   },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/headerPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <ul class="nav-elements">
+      <img src="@/img/dc-logo.png" alt="" />
+      <li>Ciao</li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "headerPage",
+};
+</script>
+
+<style lang="scss" scoped>
+$dcblue_color: #175793;
+.header {
+  color: rgb(255, 255, 255);
+  display: flex;
+  justify-content: space-between;
+  height: 20vh;
+}
+</style>
